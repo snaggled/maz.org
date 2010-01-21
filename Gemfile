2d16508abaa0e25f6e518313b32fe2c928095221
Brian Moseley <bcm@maz.org>
2010-01-20 19:04:40 -0500

    move foursquare username and password to app config file

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ bin_path 'vendor/bundler_gems/bin'
 
 gem "rails", "2.3.5"
 gem "foursquare", "0.1.0", :path => '../Foursquare'
+gem "viva-app_config", "1.2.0"
 
 except :production do
   gem "sqlite3-ruby"
--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -3,9 +3,14 @@ require 'Foursquare'
 class FoursquareController < ApplicationController
 
   def index
-    fq = Foursquare.new('bcm', 'hi')
-    @checkins = fq.history
+    @checkins = foursquare.history
   rescue Exception => e
     logger.error("Foursquare breakage: #{e.inspect}")
+  end
+
+private
+  def foursquare
+    Foursquare.new(AppConfig.foursquare.user, AppConfig.foursquare.password,
+      :headers => {'User-Agent', 'maz.org'})
   end
 end
