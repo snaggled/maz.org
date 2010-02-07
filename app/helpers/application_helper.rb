d90af9fdbec718250b2dc64382788264253150fe
Brian Moseley <bcm@maz.org>
2010-02-07 01:01:29 -0500

    add foursquare icon to checkin activities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,22 +9,22 @@ module ApplicationHelper
   end
 
   def foursquare_checkin_summary(checkin)
-    venue_link = link_to(h(checkin.venue.name),
-      "http://foursquare.com/venue/#{checkin.venue.foursquare_id}",
+    icon = image_tag('icons/foursquare.ico', :size => '16x16', :class => 'activity-icon')
+
+    venue_link = link_to(h(checkin.venue.name), "http://foursquare.com/venue/#{checkin.venue.foursquare_id}",
       :target => '_new', :class => 'foursquare-checkin-venue')
 
-    venue_location = content_tag(:span,
-      "#{h(checkin.venue.city)}, #{h(checkin.venue.state)}",
+    venue_location = content_tag(:span, "#{h(checkin.venue.city)}, #{h(checkin.venue.state)}",
       :class => 'foursquare-checkin-location')
 
     # 6:29 pm on Sat, Jan 23rd
-    ci_t = checkin.occurred_at.strftime("%I:%M %p").
-      gsub(/^0(\d)/, '\1').downcase
-    ci_d = checkin.occurred_at.
-      strftime("%a, %b #{checkin.occurred_at.day.ordinalize}")
-    checkin_time = content_tag(:span, "#{ci_t} on #{ci_d}",
-      :class => 'foursquare-checkin-time')
+    ci_t = checkin.occurred_at.strftime("%I:%M %p").gsub(/^0(\d)/, '\1').downcase
+    ci_d = checkin.occurred_at.strftime("%a, %b #{checkin.occurred_at.day.ordinalize}")
+    checkin_time = content_tag(:span, "#{ci_t} on #{ci_d}", :class => 'foursquare-checkin-time')
 
-    "Checked in at #{venue_link} in #{venue_location} at #{checkin_time}"
+    out = "#{icon} Checked in at #{venue_link} in #{venue_location} at #{checkin_time}"
+    out << " (#{shout})" if checkin.shout.present?
+
+    out
   end
 end
