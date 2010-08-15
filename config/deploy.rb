6135ba4c3495783619c282adeab21bb122f62648
Brian Moseley <bcm@maz.org>
2010-08-14 20:30:54 -0400

    update deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,18 +2,29 @@ set :application, "maz.org"
 set :repository,  "git@github.com:bcm/maz.org.git"
 set :deploy_to, "/var/www/maz"
 
-set :scm, :git
-set :use_sudo, false
-
 role :web, "maz.org"
 role :app, "maz.org"
 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+
+# XXX: symlink shared/stuff into public
+# XXX: symlink shared/production.yml into config/environments
+
+after "deploy:update_code", "deploy:unpack"
+
 namespace :deploy do
-  task :start do
+  desc 'Unpack bundled gem dependencies'
+  task :unpack, :roles => :app do
+    run "cd #{release_path} && bundle install --without=test && bundle lock"
   end
-  task :stop do
-  end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+
+  desc 'Restart Passenger'
+  task :restart, :roles => :app do
+    run "mkdir -p #{current_path}/tmp"
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
