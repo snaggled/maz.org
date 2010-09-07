9490f4debae3270bc538ee56db43e16dbf1c0a53
Brian Moseley <bcm@maz.org>
2010-09-06 21:43:22 -0700

    take stuff out of git

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,15 +12,24 @@ set :scm_verbose, true
 set :use_sudo, false
 
 # XXX: symlink shared/stuff into public
-# XXX: symlink shared/production.yml into config/environments
 
 after "deploy:update_code", "deploy:unpack"
+after "deploy:update_code", "deploy:update_config"
+after "deploy:update_code", "deploy:update_stuff"
 after "deploy", "deploy:cleanup"
 
 namespace :deploy do
   desc 'Unpack bundled gem dependencies'
   task :unpack, :roles => :app do
-    run "cd #{release_path} && bundle install --without=test && bundle lock"
+    run "cd #{release_path} && bundle install --without=test --deployment"
+  end
+
+  task :update_config, :roles => :app do
+    run "ln -nfs #{shared_path}/production.yml #{current_path}/config/environments/"
+  end
+
+  task :update_stuff, :roles => :app do
+    run "ln -nfs #{shared_path}/stuff #{current_path}/public/"
   end
 
   desc 'Restart Passenger'
